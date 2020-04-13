fc86c3fb747bf545fe9149396e4a6680a7b35785
poli <poli@orangevo>
2020-04-13 23:06:26 +0200

    text blocks with drag n drop

---
+++ frontend/opentem.js
@@ -0,0 +1,53 @@
+
+
+function allowDrop(ev) {
+	ev.preventDefault();
+}
+
+function dragStart(ev) {
+	ev.dataTransfer.setData("text/plain", ev.target.id);
+	ev.dataTransfer.dropEffect = "move";
+	//ev.target.style.display = 'none';
+}
+
+function dragEnd(ev) {
+	//ev.target.style.display = 'flex';
+}
+
+function dragover(ev) {
+	ev.preventDefault();
+	ev.dataTransfer.dropEffect = "move";
+}
+
+function drop(ev, target) {
+	ev.preventDefault();
+	var eID = ev.dataTransfer.getData("text/plain");
+	var dragged = document.getElementById(eID);
+	target.parentNode.insertBefore(dragged, target);
+}
+
+var uniqueCounter = 0;
+
+function addTextBlock(panelName) {
+	var panel = document.getElementById(panelName);
+	var block = document.createElement('div');
+	var handle = document.createElement('button');
+	var field = document.createElement('textarea');
+
+	block.className = 'block';
+	handle.className = 'handle';
+	field.className = 'field';
+
+	block.appendChild(handle);
+	block.appendChild(field);
+	panel.appendChild(block);
+
+	handle.innerHTML = '\u2022<br>\u2022<br>\u2022';
+
+	block.id = 'block'+uniqueCounter; uniqueCounter += 1;
+	block.draggable = "true";
+	block.ondragstart = dragStart;
+	block.ondragend = dragEnd;
+	block.ondragover = dragover;
+	block.ondrop = function(event) { drop(event, this); };
+}
