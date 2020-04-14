354a5111ab5bcadbb2627b29d6c31749e9952257
poli <poli@orangevo>
2020-04-14 03:35:27 +0200

    entries added

--- frontend/opentem.js
+++ frontend/opentem.js
@@ -23,7 +23,14 @@ function drop(ev, target) {
 	ev.preventDefault();
 	var eID = ev.dataTransfer.getData("text/plain");
 	var dragged = document.getElementById(eID);
-	target.parentNode.insertBefore(dragged, target);
+	if (eID == 'viewer') {
+		var block = addTextBlock('assembly');
+		block.children[0].value = dragged.children[0].value;
+		block.ID = dragged.ID;
+		target.parentNode.insertBefore(block, target);
+	} else {
+		target.parentNode.insertBefore(dragged, target);
+	}
 }
 
 var uniqueCounter = 0;
@@ -32,25 +39,61 @@ function remTextBlock(block) {
 	block.parentNode.removeChild(block);
 }
 
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+function selectEntry(entry) {
+	var viewer = document.getElementById('viewer').children[0];
+	viewer.ID = entry.ID;
+	viewer.value = entry.data;
+}
+
+function addEntry(lbl, txt, ID) {
+	var entries = document.getElementById('entries');
+	var entry = document.createElement('button');
+	entry.className = 'entry';
+	entry.innerHTML = lbl;
+	entry.data = txt;
+	entry.ID = ID;
+	entry.onclick = function() { selectEntry(entry); };
+	entries.appendChild(entry);
+}
+
+function storeBlock(block) {
+	console.log('storeBlock');
+
+	if (block.ID == undefined) block.ID = uuidv4();
+	var content = block.children[0].value;
+	var bID = block.ID;
+
+	addEntry(bID, content, bID);
+}
+
+function addBlockButton(lbl, func) {
+	var button = document.createElement('button');
+	button.className = 'newBlockButton';
+	button.innerHTML = lbl;
+	button.onmousedown = func;
+	return button;
+}
+
 function onFocusBlock(block) {
 	if (block.parentNode.id != 'assembly') return;
 
-	var buttonMinus = document.createElement('button');
-	buttonMinus.className = 'newBlockButton';
-	buttonMinus.innerHTML = '-';
-	buttonMinus.onmousedown = function() { remTextBlock(block); };
-
-	var buttonPlus = document.createElement('button');
-	buttonPlus.className = 'newBlockButton';
-	buttonPlus.innerHTML = '+';
-	buttonPlus.onmousedown = function() { addTextBlock('assembly'); };
+	var buttonMinus = addBlockButton('-', function() { remTextBlock(block); });
+	var buttonPlus  = addBlockButton('+', function() { addTextBlock('assembly'); });
+	var buttonStar  = addBlockButton('*', function() { storeBlock(block); });
 
 	var buttons = document.createElement('div');
-	if (block.parentNode.children.length > 1) {
-		buttons.appendChild(buttonMinus);
-	}
-	buttons.appendChild(buttonPlus);
 	block.parentNode.insertBefore(buttons, block.nextSibling);
+
+	if (block.parentNode.children.length > 1) buttons.appendChild(buttonMinus);
+	buttons.appendChild(buttonPlus);
+	buttons.appendChild(buttonStar);
 }
 
 function onUnfocusBlock(block) {
@@ -83,6 +126,7 @@ function addTextBlock(panelName) {
 	block.ondragover = dragover;
 	block.ondrop = function(event) { drop(event, this); };
 	//panel.ondrop = function(event) { drop(event, this); };
+	return block;
 }
 
 function compileBlocks() {
